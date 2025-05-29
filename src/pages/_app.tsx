52dd11a5a13f833060adbe9bec28839b1403588d
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-05-29 17:08:14 -0500

    wip learning ant

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import {AppProps} from "next/app";
 import {NextPage} from "next";
 import Head from "next/head";
+import {ConfigProvider} from "antd";
+
+// Import Ant Design styles
+import 'antd/dist/reset.css';
+
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: React.ReactElement) => React.ReactNode;
@@ -16,7 +21,49 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout ) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Component {...pageProps} />
+
+      <ConfigProvider theme={{
+        token: {
+          colorPrimary: '#1587b1',
+
+
+          colorBgContainer: '#ba0b0b',
+          colorBgLayout: '#e4d5ca',
+
+          colorText: '#000000',
+          colorTextSecondary: '#1587b1',
+          colorTextPlaceholder: '#1587b1',
+          colorTextDisabled: '#1587b1',
+          colorTextDescription: '#1587b1',
+
+          borderRadius: 6,
+          borderRadiusLG: 8,
+          borderRadiusSM: 4,
+          colorBorder: '#1587b1',
+
+          marginXS: 8,
+          marginSM: 12,
+          margin: 16,
+          marginMD: 20,
+          marginLG: 24,
+          marginXL: 32,
+
+
+
+        },
+        components: {
+          Layout: {
+            headerBg: 'rgba(218,170,127,0.57)',
+            footerBg: '#a67d55',
+            headerHeight: 64,
+            headerPadding: '0 50px',
+            footerPadding: '24px 50px',
+
+          }
+        }
+      }}>
+        <Component {...pageProps} />
+      </ConfigProvider>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
+import {Layout, Space, Typography} from "antd";
+
+const { Header, Content, Footer } = Layout;
+const { Title, Text } = Typography;
+
 export default function Index() {
-  return <div>Hello World</div>;
+  return (
+    <Layout>
+      <Header>
+        <Title>Caffeine Driven Development</Title>
+      </Header>
+      <Content>
+        <Space direction="vertical" size={16}>
+          <Text>This is some basic text in the content</Text>
+          {
+            Array.from({length: 100}, (_, index) => (
+              <Text>index</Text>
+            ))
+          }
+        </Space>
+      </Content>
+      <Footer>
+        <Text>This is some basic text in the footer</Text>
+      </Footer>
+    </Layout>
+  );
 }
