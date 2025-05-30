26e61c68031f00f5a5c8b83a783f8414fc03f4bf
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-05-30 14:35:04 -0500

    back in a feature branch, setting things back to a known state

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   basePath: '/caffeine-driven-development',
   transpilePackages: ['@ant-design', 'rc-util', 'rc-pagination', 'rc-picker'], // Add antd related packages
