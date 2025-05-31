9e502d6a9d5e4b698c12483c9c5f2c01fbe6cabc
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-05-31 16:18:54 -0500

    returing the project to a skeleton

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   basePath: '/caffeine-driven-development',
-  transpilePackages: ['rc-util', 'rc-picker'],
 };
 
 export default nextConfig;
