b99fb920796bd775ee0106ece5f842699fe96db5
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-05-31 17:26:16 -0500

    ugh i need to think about how to make this pretty, but for now this works

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import {AppProps} from "next/app";
 import Head from "next/head";
-import {Provider} from "@/components/ui/provider";
+import {ChakraProvider, createSystem, defaultConfig, defineConfig} from "@chakra-ui/react";
+import {ColorModeProvider} from "@/components/ui/color-mode";
+
+const config = defineConfig({
+  theme: {
+    tokens: {
+      colors:{
+        primary: { value: "#0FEE0F" },
+        secondary: { value: "#EE0F0F" },
+      }
+    },
+  }
+})
+
+const system = createSystem(defaultConfig, config)
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,9 +22,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </Head>
-      <Provider >
-        <Component {...pageProps} />
-      </Provider>
+      <ChakraProvider value={system}>
+        <ColorModeProvider>
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
-import {Button, HStack} from "@chakra-ui/react";
+import {Box, Button, VStack} from "@chakra-ui/react";
 
 export default function IndexPage() {
 
   return (
     <>
-      <HStack>
-        text text
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-      </HStack>
+      <Box color="primary" backgroundColor="secondary">
+        <VStack>
+          text text
+          <Button>Click me</Button>
+          <Button>Click me</Button>
+        </VStack>
+      </Box>
     </>
   )
 }
